fb6ff0350f796043449205b51b6b15c2df07c43a
Mike Wassmer <mikewassmer@protonmail.com>
2026-02-16 21:46:15 +0100

    fix: cast signal handler through pointer for newer clippy

--- src/status.rs
+++ src/status.rs
@@ -23,8 +23,8 @@ fn signal_i3blocks() {
 
 fn register_cleanup() {
     unsafe {
-        libc::signal(libc::SIGINT, cleanup_handler as libc::sighandler_t);
-        libc::signal(libc::SIGTERM, cleanup_handler as libc::sighandler_t);
+        libc::signal(libc::SIGINT, cleanup_handler as *const () as libc::sighandler_t);
+        libc::signal(libc::SIGTERM, cleanup_handler as *const () as libc::sighandler_t);
     }
 }
 
